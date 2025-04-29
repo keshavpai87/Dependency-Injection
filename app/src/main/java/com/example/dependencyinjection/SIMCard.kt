3b793e702ce785261904fb81f5895af951fb64ab
Keshav Pai <keshu.pai@gmail.com>
2025-04-29 10:28:42 +0530

    Function Injection

--- app/src/main/java/com/example/dependencyinjection/SIMCard.kt
+++ app/src/main/java/com/example/dependencyinjection/SIMCard.kt
@@ -2,10 +2,16 @@ package com.example.dependencyinjection
 
 import android.util.Log
 
-class SIMCard(val serviceProvider : ServiceProvider) {
+class SIMCard() {
+
+    private lateinit var serviceProvider : ServiceProvider
 
     init {
         Log.i("MyTag", "Sim Card Constructed")
+    }
+
+    fun setServiceProvider(serviceProvider: ServiceProvider) {
+        this.serviceProvider = serviceProvider
     }
 
     fun getConnection() {
